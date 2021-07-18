e99e0b0fea5443008ba3e6a3da091ec8651149b9
udeh princewill <princezubby2020@gmail.com>
2021-07-18 19:38:33 +0100

    added javascript logic

--- 01-color-flipper/setup/app.js
+++ 01-color-flipper/setup/app.js
@@ -1,1 +1,13 @@
 const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
+
+const btn = document.querySelector("#btn");
+const colorText = document.querySelector(".color")
+const body = document.querySelector("body")
+
+btn.addEventListener("click", function() {
+    const randomNumber = Math.floor(Math.random() * colors.length);
+    const randomColor = colors[randomNumber];
+
+    colorText.innerHTML = randomColor;
+    body.style.backgroundColor = randomColor;
+})
--- 01-color-flipper/setup/hex.js
+++ 01-color-flipper/setup/hex.js
@@ -1,1 +1,16 @@
 const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
+
+const btn = document.querySelector("#btn");
+const colorText = document.querySelector(".color")
+const body = document.querySelector("body")
+
+btn.addEventListener("click", function() {
+    let hexCode = "#";
+    while(hexCode.length < 7)  {
+        const randomNumber = Math.floor(Math.random() * hex.length);
+        hexCode += hex[randomNumber];
+    }
+    
+    colorText.innerText = hexCode;
+    body.style.backgroundColor = hexCode;
+})
